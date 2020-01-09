8bf59baa4fba986ee4c81f2007c4300f4bdd3cb8
Philippe Bérard <philippe.berard@jalios.com>
2020-01-09 18:13:16 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-	  Coucou : test !!!
+	  Coucou : branche Feature !!!
         </p>
         <a
           className="App-link"
